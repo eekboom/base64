7223d73e3010b927027d5ee7919e5c921d14591b
Stephen Friedrich <stephen.friedrich@mgm-tp.com>
2021-12-26 23:44:51 +0100

    character encodings first draft

--- src/Base64.tsx
+++ src/Base64.tsx
@@ -1,29 +1,47 @@
+import {decode, encode } from "iconv-lite";
 import {useState} from "preact/compat";
 
 type MyProps = {}
 
 function Base64(props: MyProps) {
-    const [myValue, setMyValue] = useState("");
+    const [textInputValue, setTextInputValue] = useState("");
+    const [encodingName, setEncodingName] = useState("UTF-8");
 
     const handleChange = (event) => {
-        setMyValue(event.target.value);
+        setTextInputValue(event.target.value);
     }
 
-    const base64Encode = (text: string): string => {
+    const base64Encode = (text: string, encodingName: string): string => {
         try {
-            return btoa(text);
+            const data: Buffer = encode(text, encodingName);
+            const decodedString = decode(data, encodingName);
+            if (text != decodedString) {
+                return `Input contains characters outside of ${encodingName}`;
+            }
+            const binaryString = data.toString("binary");
+            return btoa(binaryString);
         } catch (e) {
-            // TODO add encoding, see for example https://github.com/ashtuchkin/iconv-lite
-            return "< String contains characters outside of ISO-8859-1 >";
+            return `Input contains characters outside of ${encodingName}`;
         }
     }
 
     return <>
-        <div>Input Text (only characters from ISO-8859-1 allowed)</div>
-        <textarea value={myValue} onChange={handleChange} cols={60}/>
+        <div>Input Text</div>
+        <textarea value={textInputValue} onChange={handleChange} cols={60}/>
+
+        <div>Encoding:</div>
+        <select value={encodingName} onChange={(event) => setEncodingName(event.target.value)}>
+            <option>UTF-8</option>
+            <option>ISO-8859-15</option>
+            <option>ISO-8859-1</option>
+            <option>ASCII</option>
+            <option>win1251</option>
+            <option>UTF-16LE</option>
+            <option>UTF-16BE</option>
+        </select>
 
         <div>Base 64:</div>
-        <div id="base64-output">{base64Encode(myValue)}</div>
+        <div id="base64-output">{base64Encode(textInputValue, encodingName)}</div>
     </>;
 }
 
