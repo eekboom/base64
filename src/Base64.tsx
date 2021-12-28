f1da91214ad40e500f6941d08aebe4b48dbdaa62
Stephen Friedrich <stephen.friedrich@mgm-tp.com>
2021-12-28 23:20:22 +0100

    adds option to wrap output

--- src/Base64.tsx
+++ src/Base64.tsx
@@ -1,14 +1,34 @@
-import {decode, encode } from "iconv-lite";
+import {decode, encode} from "iconv-lite";
 import {useState} from "preact/compat";
 
 type MyProps = {}
 
 function Base64(props: MyProps) {
     const [textInputValue, setTextInputValue] = useState("");
+    const [wrapOutput, setWrapOutput] = useState(false);
     const [encodingName, setEncodingName] = useState("UTF-8");
+    const wrapAt = 76;
 
-    const handleChange = (event) => {
+    function handleInputChanged(event) {
         setTextInputValue(event.target.value);
+    }
+
+    function handleEncodingChanged(event) {
+        setEncodingName(event.target.value);
+    }
+
+    function handleWrapOutputChanged(event) {
+        setWrapOutput(!wrapOutput);
+    }
+
+    function wrap(text: string, wrapAt) {
+        let result = "";
+        let sep = ""
+        for(let i = 0; i < text.length; i += wrapAt) {
+            result += sep + text.substring(i, Math.min(i + wrapAt, text.length));
+            sep = "\n";
+        }
+        return result;
     }
 
     const base64Encode = (text: string, encodingName: string): string => {
@@ -19,7 +39,8 @@ function Base64(props: MyProps) {
                 return `Input contains characters outside of ${encodingName}`;
             }
             const binaryString = data.toString("binary");
-            return btoa(binaryString);
+            const base64 = btoa(binaryString);
+            return wrapOutput ? wrap(base64, wrapAt) : base64;
         } catch (e) {
             return `Input contains characters outside of ${encodingName}`;
         }
@@ -27,21 +48,26 @@ function Base64(props: MyProps) {
 
     return <>
         <div>Input Text</div>
-        <textarea value={textInputValue} onChange={handleChange} cols={60}/>
+        <textarea value={textInputValue} onChange={handleInputChanged} cols={80}/>
 
         <div>Encoding:</div>
-        <select value={encodingName} onChange={(event) => setEncodingName(event.target.value)}>
+        <select value={encodingName} onChange={handleEncodingChanged}>
             <option>UTF-8</option>
             <option>ISO-8859-15</option>
             <option>ISO-8859-1</option>
             <option>ASCII</option>
             <option>win1251</option>
+            <option>win1252</option>
             <option>UTF-16LE</option>
             <option>UTF-16BE</option>
+            <option>UTF-32LE</option>
+            <option>UTF-32BE</option>
         </select>
 
         <div>Base 64:</div>
-        <div id="base64-output">{base64Encode(textInputValue, encodingName)}</div>
+        <input id="wrap-output" type="checkbox" checked={wrapOutput} onClick={handleWrapOutputChanged}/>
+        <label for="wrap-output">Wrap at 76 characters</label>
+        <div><textarea id="base64-output" value={base64Encode(textInputValue, encodingName)} cols={80} readonly="readonly"/></div>
     </>;
 }
 
