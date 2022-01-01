dab9e07b59ddae567ed1b9a0f1d23e3316c5b06c
Stephen Friedrich <stephen.friedrich@mgm-tp.com>
2022-01-01 12:37:04 +0100

    adds eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Base64 from "./Base64";
+import {JSX} from "preact";
 
-export const App = () => {
+export const App = (): JSX.Element => {
     return (
         <div>
             <h1>Base 64 Encoder</h1>
