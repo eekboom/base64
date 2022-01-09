74e107f67db63ee1530ceb5fda35ec26b35c67ef
Stephen Friedrich <stephen.friedrich@eekboom.de>
2022-01-09 22:14:07 +0100

    basic styling (#33)

--- src/Base64.tsx
+++ src/Base64.tsx
@@ -44,38 +44,47 @@ function Base64(): JSX.Element {
         setSeparatedLines(newSeparatedLines)
     }
 
-    return <>
-        <div>Input Text</div>
-        <textarea value={textInputValue}
-                  onInput={(event: TargetedEvent<HTMLTextAreaElement>): void => setTextInputValue(event.currentTarget.value)}
-                  cols={80}/>
+    return (
+        <form id="base64">
+            <header>Base 64 Encoder</header>
 
-        <div>Encoding:</div>
-        <select value={encodingName}
-                onChange={(event: TargetedEvent<HTMLSelectElement>): void => setEncodingName(event.currentTarget.value)}>
-            <option>UTF-8</option>
-            <option>ISO-8859-15</option>
-            <option>ISO-8859-1</option>
-            <option>ASCII</option>
-            <option>win1251</option>
-            <option>win1252</option>
-            <option>UTF-16LE</option>
-            <option>UTF-16BE</option>
-            <option>UTF-32LE</option>
-            <option>UTF-32BE</option>
-        </select>
+            <label htmlFor="text-input">Input Text</label>
+            <textarea id="text-input"
+                      value={textInputValue}
+                      onInput={(event: TargetedEvent<HTMLTextAreaElement>): void => setTextInputValue(event.currentTarget.value)}
+                      spellcheck={false}/>
 
-        <div>Base 64:</div>
-        <div>
-            <input id="wrap-output" type="checkbox" checked={wrapOutput} onClick={handleWrapOutputClicked}/>
-            <label for="wrap-output">Wrap at 76 characters</label>
-        </div>
-        <div>
+            <label htmlFor="encoding-select">Encoding</label>
+            <select id="encoding-select"
+                    value={encodingName}
+                    onChange={(event: TargetedEvent<HTMLSelectElement>): void => setEncodingName(event.currentTarget.value)}>
+                <option>UTF-8</option>
+                <option>ISO-8859-15</option>
+                <option>ISO-8859-1</option>
+                <option>ASCII</option>
+                <option>win1251</option>
+                <option>win1252</option>
+                <option>UTF-16LE</option>
+                <option>UTF-16BE</option>
+                <option>UTF-32LE</option>
+                <option>UTF-32BE</option>
+            </select>
+
+            <label htmlFor="encode-lines">Encode each line separately</label>
             <input id="encode-lines" type="checkbox" checked={separatedLines} onClick={handleSeparatedLinesClicked}/>
-            <label for="encode-lines">Encode each line separately</label>
-        </div>
-        <div><textarea id="base64-output" value={encodeTextInput()} cols={80} readonly/></div>
-    </>;
+
+            <label htmlFor="wrap-output">Wrap at {wrapAt} characters</label>
+            <input id="wrap-output" type="checkbox" checked={wrapOutput} onClick={handleWrapOutputClicked}/>
+
+            <hr/>
+
+            <label htmlFor="base64-output">Base 64</label>
+            <textarea id="base64-output"
+                      value={encodeTextInput()}
+                      readonly
+                      spellcheck={false}/>
+        </form>
+    );
 }
 
 export default Base64;
